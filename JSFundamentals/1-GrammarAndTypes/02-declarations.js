74997ca18fe8a057daf1facbced6e4b4a4d70699
Adam Christopher Arnold <adamchristopherarnold@Adams-MacBook-Pro.local>
2020-03-17 14:17:53 -0400

    initial

---
+++ JSFundamentals/1-GrammarAndTypes/02-declarations.js
@@ -0,0 +1,37 @@
+//variables
+
+var a=1;
+    var  b    =     2;
+//  (1) (2)  (3)    (4)
+
+/*
+1- var keyword (denotes a variable)
+2- variable name (cannot be a reserved word, or start with a number)
+3- assignment operaator 
+4- value put inside the variable
+*/
+
+// Varable Declaration
+var stream;
+
+//Variable Initialization
+stream = true;
+stream = false; //re-initialization
+
+//var, let, const
+let today ='great';
+const efa = 'School';
+
+today = 'mediocre';
+//efa = 'Consulting';
+
+console.log(today, efa);
+
+
+
+
+
+
+
+
+
---
+++ JSFundamentals/1-GrammarAndTypes/03-types.js
@@ -0,0 +1,77 @@
+//Boolean
+let on = true;
+console.log(on);
+
+let off = false;
+console.log(off);
+
+//Null
+let empty = null;
+
+//Undefined
+let walmart = undefined;
+//Same as let walmart;
+
+//Numbers
+
+let degrees = 90;
+
+let precise = 999999999999999;
+console.log(precise);
+
+let toomuch = 9999999999999999n;
+console.log(toomuch);
+
+let notQuite = 0.2 + 0.1;
+console.log(notQuite);
+
+console.log(typeof toomuch);
+
+//String
+let stringOne = 'single quotes';
+let stringTwo = "double quotes";
+let stringThree = `backticks`;
+
+//Numbers vs Strings
+//addition
+let first = 1050 + 100;
+//strign concatination
+let second = '1050' + '100';
+
+console.log(first, second);
+
+let third = 1050 + '100';
+console.log(third);
+console.log(typeof third);
+
+const firstName = 'Adam';
+let lastName = 'Arnold';
+let houseNumber = 1150;
+let street = 'w 116th st';
+let city = 'Carmel';
+let state = 'IN';
+let zipCode = 46032;
+ 
+let address = firstName + ' ' + lastName + ', ' + houseNumber + ' ' + street + ' ' + city + ' ' + state + ' ' + zipCode + ' '
+console.log(address);
+//Object
+let firstObject = {
+    integer: 50,
+    stringOne: 'hello world',
+    isRemote: true
+};
+
+console.log(firstObject);
+
+//Array
+
+let firstArray = ['spanish', 'english', 'dutch', 'french'];
+
+console.log(firstArray[0]);
+
+
+
+
+
+
+
