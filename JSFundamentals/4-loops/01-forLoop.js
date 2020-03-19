14959ed60c7a6a2d42ad822d1c156d1f899b2df3
Adam Christopher Arnold <adamchristopherarnold@Adams-MacBook-Pro.local>
2020-03-19 08:49:08 -0400

    pure frustration

---
+++ JSFundamentals/4-loops/01-forLoop.js
@@ -0,0 +1,17 @@
+for (let i = 0; i <= 20; i+=2) {
+    console.log(i);
+}
+
+
+let name = `Ada`;
+
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+  }
+
+  
+  
---
+++ JSFundamentals/4-loops/02-forInLoop.js
@@ -0,0 +1,22 @@
+const forLoop = function() {
+    console.log(i);
+};
+
+for ( let i = 0; i < 11; i++ ) {
+return (i);
+};
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+runLoop();
+
+function myFunc() {
+    console.log(i);
+};
